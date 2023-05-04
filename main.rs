32d74bf3757e18590b208ea6f3aa45b9df97cfcf
alecascencio <132581299+alecascencio@users.noreply.github.com>
2023-05-04 09:28:40 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world! Alec here!");
+}
+
+// Cargo Tool
+
+// Create a new project: cargo new "project"
+// Build new project: cargo build
+// Run and compile your project: cargo run
+// Check your Rust file: cargo check
